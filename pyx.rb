25694fd8b535b7ff22bf7f49e6e929dc262e28cf
chi <chi@app4j.org>
2021-04-11 12:32:21 +0800

    Upgrade to 1.0.4

--- pyx.rb
+++ pyx.rb
@@ -4,8 +4,8 @@
 class Pyx < Formula
   desc "Single command to run python3 script anywhere."
   homepage "https://github.com/darumatic/pyx/releases/"
-  url "https://github.com/darumatic/pyx/releases/download/1.0.3/pyx_1.0.3_Darwin_x86_64.tar.gz"
-  sha256 "e16bf0c9c89a944bfff51499647af5f100008712621bed1a6f4d09e1afd17f11"
+  url "https://github.com/darumatic/pyx/releases/download/1.0.4/pyx_1.0.4_Darwin_x86_64.tar.gz"
+  sha256 "cf9406c08770255069bd74e7b3b1a3a3cc0778894e4957ce2577f69b1afe4f10"
   license "GPL-3.0"
 
   bottle :disable, "Use pyx prebuild binary"
